d48ce64814e3676bbb60eaff782275080b06771e
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2021-05-04 15:39:26 +0000

    Add program to calculate closest NL sphere to the border

---
+++ closest-nl-to-border.py
@@ -0,0 +1,30 @@
+from BorderModel import BorderModel
+from BorderModel import distance_to_line
+
+class CsvWriter:
+	def __init__(self, filename):
+		self.filename = filename
+		self.file = open(filename, "w")
+
+		self.file.write("name;distance;sound_mean_zero\n")
+
+	def write_line(self, name, distance, sound_mean_zero):
+		self.file.write("{};{};{}\n".format(name, distance, sound_mean_zero))
+
+	def close(self):
+		self.file.close()
+
+# Instantiate a model
+model = BorderModel(width=100, height=240, border_heights=[ 124, 104 ])
+
+csv_writer = CsvWriter("nl_towns.csv")
+
+# Go over every influence sphere...
+for influence_sphere in model.influence_spheres:
+	if influence_sphere.country == "The Netherlands":
+		distance = distance_to_line(model.border_coords[0], model.border_coords[1], (influence_sphere.x, influence_sphere.y))
+		sound_mean_zero = "1" if influence_sphere.sound_mean == 0 else "0"
+
+		csv_writer.write_line(influence_sphere.name, distance, sound_mean_zero)
+
+csv_writer.close()
