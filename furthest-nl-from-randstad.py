ec074f4de58791d281c9b5aa3ab7bd6d3df20da4
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2021-05-05 17:22:20 +0000

    Change selection principles for NL tracking sphere

--- furthest-nl-from-randstad.py
+++ furthest-nl-from-randstad.py
@@ -1,5 +1,5 @@
 from BorderModel import BorderModel
-from BorderModel import distance_to_line
+from BorderModel import distance_between_points
 
 class CsvWriter:
 	def __init__(self, filename):
@@ -22,7 +22,7 @@ csv_writer = CsvWriter("nl_towns.csv")
 # Go over every influence sphere...
 for influence_sphere in model.influence_spheres:
 	if influence_sphere.country == "The Netherlands":
-		distance = distance_to_line(model.border_coords[0], model.border_coords[1], (influence_sphere.x, influence_sphere.y))
+		distance = distance_between_points(47, influence_sphere.x, 32, influence_sphere.y)
 		sound_mean_zero = "1" if influence_sphere.sound_mean == 0 else "0"
 
 		csv_writer.write_line(influence_sphere.name, distance, sound_mean_zero)
