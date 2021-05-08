0cff94af79b4382366215cfd79cc1019213a02ed
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2021-05-08 13:35:22 +0000

    Revamped BorderThink: now supports multiple stages for the same theory

--- BorderThink.py
+++ BorderThink.py
@@ -10,6 +10,7 @@ from BorderModel import BorderModel
 parser = argparse.ArgumentParser(description='BorderThink automates the different parameters for the BorderModel simulation')
 parser.add_argument('theory', type=str, help="Which theory do you want to test?\
 					contact - target - ethnocentrism - scaled_ethnocentrism - media")
+parser.add_argument('stage', type=int, help="Which stage do you want to simulate? (there are multiple follow-up models) 1, 2 ..")
 parser.add_argument('iterations', type=int, help='How many times should each variable parameter be tested?')
 parser.add_argument('max_steps', type=int, help='What is the step ceiling for this model?')
 
@@ -35,9 +36,14 @@ if args.theory == "contact":
 					 "scaled_ethnocentrism": False,
 					 "media_receptiveness": False }
 
-	parameters_list = [ { "abroad_travel_chance_nl": 1 / pow(10, power),
-						   "abroad_travel_chance_be": 1 / pow(10, power) } \
-						   for power in [0] + numpy.arange(1, 8, 1) ]
+	if args.stage == 1: # range: 0 -> 0.0100, step = 0.0010
+		parameters_list = [ { "abroad_travel_chance_nl": probability,
+						   "abroad_travel_chance_be": probability } \
+						   for probability in numpy.arange(0.0000, 0.0101, 0.001) ]
+	elif args.stage == 2: # range: 1/10 -> 1/10^7
+		parameters_list = [ { "abroad_travel_chance_nl": 1 / pow(10, power),
+							   "abroad_travel_chance_be": 1 / pow(10, power) } \
+							   for power in [0] + numpy.arange(1, 8, 1) ]
 elif args.theory == "target":
 	fixed_params = { **fixed_params,
 					 "abroad_travel_chance_be": 0.001,
@@ -48,20 +54,31 @@ elif args.theory == "target":
 					 "scaled_ethnocentrism": False,
 					 "media_receptiveness": False }
 
+	print("Stage is currently unsupported for this theory and will be ignored")
+
+	# range: 0 -> 0.100, step = 0.010
 	parameters_list = [ { "domestic_travel_chance_nl": probability } \
-						   for probability in numpy.arange(0.000, 1, 0.100) ]
+						   for probability in numpy.arange(0.000, 0.100, 0.010) ]
 elif args.theory == "ethnocentrism":
 	fixed_params = { **fixed_params,
-					 "abroad_travel_chance_be": 0.001,
-					 "abroad_travel_chance_nl": 0.001,
 					 "domestic_travel_chance_be": 0.005,
 					 "domestic_travel_chance_nl": 0.005,
 					 "scaled_ethnocentrism": False,
 					 "media_receptiveness": False }
 
-	parameters_list = [ { "ethnocentrism_nl": 0.85,
-						  "ethnocentrism_be": 1 / pow(10, power) } \
-						   for power in [0] + numpy.arange(1, 8, 1) ]
+
+	if args.stage == 1:
+		parameters_list = [ { "abroad_travel_chance_be": 0.001,
+						 	  "abroad_travel_chance_nl": 0.001,
+							  "ethnocentrism_nl": 0.85,
+							  "ethnocentrism_be": probability } \
+							   for probability in numpy.arange(0, 1.01, 0.05) ]
+	elif args.stage == 2:
+		parameters_list = [ { "abroad_travel_chance_be": 0.0001,
+						 	  "abroad_travel_chance_nl": 0.0001,
+							  "ethnocentrism_nl": 0.85,
+							  "ethnocentrism_be": probability } \
+							   for probability in numpy.arange(0, 1.01, 0.05) ]
 elif args.theory == "scaled_ethnocentrism":
 	fixed_params = { **fixed_params,
 					 "abroad_travel_chance_be": 0.001,
@@ -71,8 +88,9 @@ elif args.theory == "scaled_ethnocentrism":
 					 "media_receptiveness": False,
 					 "ethnocentrism_nl": 0,
 					 "ethnocentrism_be": 0, }
-
 	parameters_list = [ { "scaled_ethnocentrism": True } ]
+
+	print("Stage is unsupported for this theory and will be ignored")
 elif args.theory == "media":
 	fixed_params = { **fixed_params,
 					 "abroad_travel_chance_be": 0.001,
@@ -83,8 +101,10 @@ elif args.theory == "media":
 					 "ethnocentrism_nl": 0,
 					 "ethnocentrism_be": 0, }
 
-	parameters_list = [ { "media_receptiveness": 1 / pow(10, power) } \
-						   for power in [0] + numpy.arange(1, 8, 1) ]
+	print("Stage is currently unsupported for this theory and will be ignored")
+
+	parameters_list = [ { "media_receptiveness": probability } \
+						   for probability in numpy.arange(0, 1.05, 0.05) ]
 # This theory is meant to test whether language change is accelerated when we increase
 # the domestic travel chance for the Netherlands
 elif args.theory == "nl_artefact_test":
